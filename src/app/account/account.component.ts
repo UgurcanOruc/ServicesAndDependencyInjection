a5b1307d3711868894ee4bde019520d87bdfdac0
UgurcanOruc <ugurcan@abisteknoloji.com.tr>
2022-06-02 17:16:33 +0300

    * Logging and Accounts services are added. ** Dependancy injections are used. *** Events and variables that passed between components are now be doing with serveces.

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,24 +1,22 @@
-import { Component, EventEmitter, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AccountsService } from '../accounts.service';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
-export class AccountComponent implements OnInit {
+export class AccountComponent {
 
   @Input() account!: { name: string, status: string };
   @Input() id!:number;
-  @Input() statusChanged = new EventEmitter<{ id: number, newStatus: string }>();
     
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService) { }
+
   onSetTo(status: string) {
-    this.statusChanged.emit({ id: this.id, newStatus: status });
-    console.log('A server status changed, new status: ' + status);
-  }
-
-  constructor() { }
-
-  ngOnInit(): void {
+    this.accountsService.updateStatus(this.id, status);
+    this.accountsService.statusUpdated.emit(status);
   }
 
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,25 +1,21 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { AccountsService } from '../accounts.service';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css']
 })
-export class NewAccountComponent implements OnInit {
+export class NewAccountComponent {
 
-  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService ) {
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountsService.addAccount(accountName,accountStatus);
   }
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
